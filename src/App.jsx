f440e83628cbe2de9b26f60a4abff114459cec75
DickyAnonym12 <dicky23si@mahasiswa.pcr.ac.id>
2025-06-18 23:31:16 +0700

    atur protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./assets/tailwind.css";
 import Loading from "./components/Loading";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import React, { Suspense } from "react";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import GuestLayout from "./layouts/GuestLayout";
 import AuthLayout from "./layouts/AuthLayout";
@@ -35,43 +37,53 @@ const Membership = React.lazy(() => import("./pages/MemberShip"));
 
 function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        {/* Main Routes */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<DashboardAdmin />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/pelanggan" element={<Pelanggan />} /> 
-          <Route path="/pelanggan/:id" element={<DetailPelanggan />} />
-          <Route path="/pesanan-butik" element={<PesananButik />} />
-          <Route path="/pesanan-butik/:id" element={<DetailPesananButik />} />
-          <Route path="/laporan-penjualan" element={<LaporanPenjualan />} />
-          <Route path="/faq-admin" element={<FaqAdmin />} />
-          <Route path="/newsletter-admin" element={<NewsletterAdmin />} />
-          <Route path="/newsletter-campaigns" element={<NewsletterCampaigns />} />
-          <Route path="/newsletter-campaigns/add" element={<AddCampaignForm />} />
-          <Route path="/newsletter-campaigns/:id/edit" element={<EditCampaignForm />} />
-          <Route path="/contact" element={<Error404 />} />
-          <Route path="/membership" element={<Membership />} />
-        </Route>
+    <AuthProvider>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* Auth Routes (Public) */}
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot" element={<Forgot />} />
+          </Route>
 
-        {/* Guest Routes */}
-        <Route element={<GuestLayout />}>
-          <Route path="/guest" element={<Dashboard />} />
-        </Route>
+          {/* Protected Admin Routes */}
+          <Route element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <MainLayout />
+            </ProtectedRoute>
+          }>
+            <Route path="/" element={<DashboardAdmin />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/pelanggan" element={<Pelanggan />} /> 
+            <Route path="/pelanggan/:id" element={<DetailPelanggan />} />
+            <Route path="/pesanan-butik" element={<PesananButik />} />
+            <Route path="/pesanan-butik/:id" element={<DetailPesananButik />} />
+            <Route path="/laporan-penjualan" element={<LaporanPenjualan />} />
+            <Route path="/faq-admin" element={<FaqAdmin />} />
+            <Route path="/newsletter-admin" element={<NewsletterAdmin />} />
+            <Route path="/newsletter-campaigns" element={<NewsletterCampaigns />} />
+            <Route path="/newsletter-campaigns/add" element={<AddCampaignForm />} />
+            <Route path="/newsletter-campaigns/:id/edit" element={<EditCampaignForm />} />
+            <Route path="/contact" element={<Error404 />} />
+            <Route path="/membership" element={<Membership />} />
+          </Route>
 
-        {/* Auth Routes */}
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot" element={<Forgot />} />
-        </Route>
+          {/* Protected User Routes */}
+          <Route element={
+            <ProtectedRoute allowedRoles={['user']}>
+              <GuestLayout />
+            </ProtectedRoute>
+          }>
+            <Route path="/guest" element={<Dashboard />} />
+          </Route>
 
-        {/* Catch all */}
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </Suspense>
+          {/* Catch all - redirect to login */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Suspense>
+    </AuthProvider>
   );
 }
 
